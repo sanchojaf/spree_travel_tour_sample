08ef618aa98a96edf5bf663156d6ee71f0c9443c
Miguel Sancho <sanchojaf@gmail.com>
2013-12-25 23:46:07 -0500

    added Module SpreeTravelTourSanple

--- lib/spree_travel_tour_sample.rb
+++ lib/spree_travel_tour_sample.rb
@@ -1,54 +1,54 @@
 require 'spree_core'
 #require 'spree_travel_core'
-#require 'spree/travel_sample'
+#require 'spree/travel_tour_sample'
 
-module SpreeTravelSample
+module SpreeTravelTourSample
   class Engine < Rails::Engine
-    engine_name 'spree_travel_sample'
+    engine_name 'spree_travel_tour_sample'
 
     # Needs to be here so we can access it inside the tests
-    def self.load_travel_samples
+    def self.load_travel_tour_samples
         
-      Spree::TravelSample.load_travel_sample("option_types")
-      Spree::TravelSample.load_travel_sample("option_values")
+      Spree::TravelTourSample.load_travel_tour_sample("option_types")
+      Spree::TravelTourSample.load_travel_tour_sample("option_values")
 
-      Spree::TravelSample.load_travel_sample("property_types")      
-#      Spree::TravelSample.load_travel_sample("properties_flight")
-#      Spree::TravelSample.load_travel_sample("properties_hotel")
-#      Spree::TravelSample.load_travel_sample("properties_program")
-#      Spree::TravelSample.load_travel_sample("properties_rent_a_car")
-      Spree::TravelSample.load_travel_sample("properties_tour")
-#      Spree::TravelSample.load_travel_sample("properties_transfer")
+      Spree::TravelTourSample.load_travel_tour_sample("property_types")      
+#      Spree::TravelTourSample.load_travel_tour_sample("properties_flight")
+#      Spree::TravelTourSample.load_travel_tour_sample("properties_hotel")
+#      Spree::TravelTourSample.load_travel_tour_sample("properties_program")
+#      Spree::TravelTourSample.load_travel_tour_sample("properties_rent_a_car")
+      Spree::TravelTourSample.load_travel_tour_sample("properties_tour")
+#      Spree::TravelTourSample.load_travel_tour_sample("properties_transfer")
 
-      Spree::TravelSample.load_travel_sample("taxonomy_categories")      
-      Spree::TravelSample.load_travel_sample("taxon_category")
+      Spree::TravelTourSample.load_travel_tour_sample("taxonomy_categories")      
+      Spree::TravelTourSample.load_travel_tour_sample("taxon_category")
 
-#      Spree::TravelSample.load_travel_sample("taxons_hotel")
-#      Spree::TravelSample.load_travel_sample("taxons_flight")            
-#      Spree::TravelSample.load_travel_sample("taxons_program")      
-#      Spree::TravelSample.load_travel_sample("taxons_tour")
-#      Spree::TravelSample.load_travel_sample("taxons_transfer")
-#      Spree::TravelSample.load_travel_sample("taxons_car_rental")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_hotel")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_flight")            
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_program")      
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_tour")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_transfer")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_car_rental")
             
-#      Spree::TravelSample.load_travel_sample("taxonomy_destinations")
-#      Spree::TravelSample.load_travel_sample("taxons_destination")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxonomy_destinations")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_destination")
       
-#      Spree::TravelSample.load_travel_sample("taxonomy_flight_destinations")
-#      Spree::TravelSample.load_travel_sample("taxons_flight_destination")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxonomy_flight_destinations")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_flight_destination")
       
-#      Spree::TravelSample.load_travel_sample("taxonomy_things_to_do")
-#      Spree::TravelSample.load_travel_sample("taxons_thing_to_do")      
+#      Spree::TravelTourSample.load_travel_tour_sample("taxonomy_things_to_do")
+#      Spree::TravelTourSample.load_travel_tour_sample("taxons_thing_to_do")      
 
       #temporal because shipping category is a required field to Product model
-      Spree::TravelSample.load_travel_sample("shipping_categories")       
+      Spree::TravelTourSample.load_travel_tour_sample("shipping_categories")       
 
-      Spree::TravelSample.load_travel_sample("products")  
-      Spree::TravelSample.load_travel_sample("product_properties")
-      Spree::TravelSample.load_travel_sample("product_option_types")    
+      Spree::TravelTourSample.load_travel_tour_sample("products")  
+      Spree::TravelTourSample.load_travel_tour_sample("product_properties")
+      Spree::TravelTourSample.load_travel_tour_sample("product_option_types")    
 
-      Spree::TravelSample.load_travel_sample("variants")    
+      Spree::TravelTourSample.load_travel_tour_sample("variants")    
       
-      Spree::TravelSample.load_travel_sample("assets")    
+      Spree::TravelTourSample.load_travel_tour_sample("assets")    
            
 
     end
