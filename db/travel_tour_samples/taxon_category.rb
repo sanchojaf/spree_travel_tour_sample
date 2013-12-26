840f968c7f5b2e5973ecd34b53944a8856150536
Miguel Sancho <sanchojaf@gmail.com>
2013-12-26 00:24:17 -0500

    changed category taxonomy

--- db/travel_tour_samples/taxon_category.rb
+++ db/travel_tour_samples/taxon_category.rb
@@ -8,44 +8,152 @@ taxons = [
   position: 0 
  },
  {
-  name: 'Eat out',
+  name: 'Adventure',
   taxonomy: categories,
-  permalink: 'categories/eat-out',
+  permalink: 'categories/adventure',
   position: 1
  },
  {
-  name: 'Drinks',
+  name: 'Sport',
   taxonomy: categories,
-  permalink: 'categories/drinks',
+  permalink: 'categories/adventure/sport',
+  position: 2
+ },
+ {
+  name: 'Backcountry Skiing',
+  taxonomy: categories,
+  permalink: 'categories/adventure/sport/backcountry_skiing',
+  position: 3
+ },
+ {
+  name: 'Surfing',
+  taxonomy: categories,
+  permalink: 'categories/adventure/sport/surfing',
+  position: 3
+ },
+ {
+  name: 'Swimming',
+  taxonomy: categories,
+  permalink: 'categories/adventure/sport/swimming',
+  position: 3
+ },
+ {
+  name: 'Waterskiing',
+  taxonomy: categories,
+  permalink: 'categories/adventure/sport/waterskiing',
+  position: 3
+ },
+ {
+  name: 'Sailing',
+  taxonomy: categories,
+  permalink: 'categories/adventure/sport/sailing',
+  position: 3
+ },
+ {
+  name: 'Wakeboarding',
+  taxonomy: categories,
+  permalink: 'categories/adventure/sport/wakeboarding',
+  position: 3
+ },
+ {
+  name: 'Mountain Biking',
+  taxonomy: categories,
+  permalink: 'categories/adventure/sport/mountain_biking',
+  position: 3
+ },
+ {
+  name: 'Boat Excursion',
+  taxonomy: categories,
+  permalink: 'categories/adventure/boat_excursion',
+  position: 2
+ },
+ {
+  name: 'Desert Excursion',
+  taxonomy: categories,
+  permalink: 'categories/adventure/desert_excursion',
+  position: 2
+ },
+ {
+  name: 'Fishing',
+  taxonomy: categories,
+  permalink: 'categories/adventure/fishing',
+  position: 2
+ },
+ {
+  name: 'Hiking',
+  taxonomy: categories,
+  permalink: 'categories/adventure/hiking',
+  position: 2
+ },
+ {
+  name: 'Hunting',
+  taxonomy: categories,
+  permalink: 'categories/adventure/hunting',
+  position: 2
+ },
+ {
+  name: 'Safari',
+  taxonomy: categories,
+  permalink: 'categories/adventure/safari',
+  position: 2
+ },
+ {
+  name: 'Scuba Diving',
+  taxonomy: categories,
+  permalink: 'categories/adventure/scuba_diving',
+  position: 2
+ },
+ {
+  name: 'Recreation',
+  taxonomy: categories,
+  permalink: 'categories/recreation',
   position: 1
  },
  {
-  name: 'Nightlife',
+  name: 'Dancing',
   taxonomy: categories,
-  permalink: 'categories/nightlife',
+  permalink: 'categories/recreation/dancing',
+  position: 2
+ },
+{
+  name: 'Sightseeing',
+  taxonomy: categories,
+  permalink: 'categories/recreation/sightseeing',
+  position: 2
+ },
+ {
+  name: 'Relaxation',
+  taxonomy: categories,
+  permalink: 'categories/relaxation',
   position: 1
  },
  {
-  name: 'Shopping',
+  name: 'Tai Chi',
   taxonomy: categories,
-  permalink: 'categories/shopping',
-  position: 1
+  permalink: 'categories/relaxation/tai_chi',
+  position: 2
+ },
+{
+  name: 'Yoga',
+  taxonomy: categories,
+  permalink: 'categories/relaxation/yoga',
+  position: 2
  },
  {
-  name: 'Beaches',
+  name: 'Reiki',
   taxonomy: categories,
-  permalink: 'categories/beaches',
-  position: 1
+  permalink: 'categories/relaxation/reiki',
+  position: 2
  },
  {
-  name: 'Interesting places',
+  name: 'Pilates',
   taxonomy: categories,
-  permalink: 'categories/interesting-places',
-  position: 1
- },
+  permalink: 'categories/relaxation/pilates',
+  position: 2
+ }
 ]    
 
-
+     
 taxons.each do |taxon_attrs|
   taxon = Spree::Taxon.find_or_create_by_permalink(taxon_attrs[:permalink])
   taxon.taxonomy = taxon_attrs[:taxonomy]
