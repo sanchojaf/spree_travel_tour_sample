6f4c5b62c14953907efc207fd4c315c83db0b4a0
Miguel Sancho <sanchojaf@gmail.com>
2013-12-25 21:51:10 -0500

    change for tour

--- db/travel_samples/taxon_category.rb
+++ db/travel_samples/taxon_category.rb
@@ -7,7 +7,45 @@ taxons = [
   permalink: 'categories',
   position: 0 
  },
+ {
+  name: 'Eat out',
+  taxonomy: categories,
+  permalink: 'categories/eat-out',
+  position: 1
+ },
+ {
+  name: 'Drinks',
+  taxonomy: categories,
+  permalink: 'categories/drinks',
+  position: 1
+ },
+ {
+  name: 'Nightlife',
+  taxonomy: categories,
+  permalink: 'categories/nightlife',
+  position: 1
+ },
+ {
+  name: 'Shopping',
+  taxonomy: categories,
+  permalink: 'categories/shopping',
+  position: 1
+ },
+ {
+  name: 'Beaches',
+  taxonomy: categories,
+  permalink: 'categories/beaches',
+  position: 1
+ },
+ {
+  name: 'Interesting places',
+  taxonomy: categories,
+  permalink: 'categories/interesting-places',
+  position: 1
+ },
 ]    
+
+
 taxons.each do |taxon_attrs|
   taxon = Spree::Taxon.find_or_create_by_permalink(taxon_attrs[:permalink])
   taxon.taxonomy = taxon_attrs[:taxonomy]
--- lib/spree_travel_sample.rb
+++ lib/spree_travel_sample.rb
@@ -13,31 +13,31 @@ module SpreeTravelSample
       Spree::TravelSample.load_travel_sample("option_values")
 
       Spree::TravelSample.load_travel_sample("property_types")      
-      Spree::TravelSample.load_travel_sample("properties_flight")
-      Spree::TravelSample.load_travel_sample("properties_hotel")
-      Spree::TravelSample.load_travel_sample("properties_program")
-      Spree::TravelSample.load_travel_sample("properties_rent_a_car")
+#      Spree::TravelSample.load_travel_sample("properties_flight")
+#      Spree::TravelSample.load_travel_sample("properties_hotel")
+#      Spree::TravelSample.load_travel_sample("properties_program")
+#      Spree::TravelSample.load_travel_sample("properties_rent_a_car")
       Spree::TravelSample.load_travel_sample("properties_tour")
-      Spree::TravelSample.load_travel_sample("properties_transfer")
+#      Spree::TravelSample.load_travel_sample("properties_transfer")
 
       Spree::TravelSample.load_travel_sample("taxonomy_categories")      
       Spree::TravelSample.load_travel_sample("taxon_category")
 
-      Spree::TravelSample.load_travel_sample("taxons_hotel")
-      Spree::TravelSample.load_travel_sample("taxons_flight")            
-      Spree::TravelSample.load_travel_sample("taxons_program")      
-      Spree::TravelSample.load_travel_sample("taxons_tour")
-      Spree::TravelSample.load_travel_sample("taxons_transfer")
-      Spree::TravelSample.load_travel_sample("taxons_car_rental")
+#      Spree::TravelSample.load_travel_sample("taxons_hotel")
+#      Spree::TravelSample.load_travel_sample("taxons_flight")            
+#      Spree::TravelSample.load_travel_sample("taxons_program")      
+#      Spree::TravelSample.load_travel_sample("taxons_tour")
+#      Spree::TravelSample.load_travel_sample("taxons_transfer")
+#      Spree::TravelSample.load_travel_sample("taxons_car_rental")
             
-      Spree::TravelSample.load_travel_sample("taxonomy_destinations")
-      Spree::TravelSample.load_travel_sample("taxons_destination")
+#      Spree::TravelSample.load_travel_sample("taxonomy_destinations")
+#      Spree::TravelSample.load_travel_sample("taxons_destination")
       
-      Spree::TravelSample.load_travel_sample("taxonomy_flight_destinations")
-      Spree::TravelSample.load_travel_sample("taxons_flight_destination")
+#      Spree::TravelSample.load_travel_sample("taxonomy_flight_destinations")
+#      Spree::TravelSample.load_travel_sample("taxons_flight_destination")
       
-      Spree::TravelSample.load_travel_sample("taxonomy_things_to_do")
-      Spree::TravelSample.load_travel_sample("taxons_thing_to_do")      
+#      Spree::TravelSample.load_travel_sample("taxonomy_things_to_do")
+#      Spree::TravelSample.load_travel_sample("taxons_thing_to_do")      
 
       #temporal because shipping category is a required field to Product model
       Spree::TravelSample.load_travel_sample("shipping_categories")       
