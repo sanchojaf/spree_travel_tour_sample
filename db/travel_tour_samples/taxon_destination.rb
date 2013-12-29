8985516ada95c18147d41b3e0c749b3f28eb46b7
Miguel Sancho <sanchojaf@gmail.com>
2013-12-29 11:47:56 -0500

    change in destinatination taxons

--- db/travel_tour_samples/taxon_destination.rb
+++ db/travel_tour_samples/taxon_destination.rb
@@ -39,6 +39,10 @@ taxons = [
       products[:ror_bag],
     ]
   },
+
+]
+
+taxons_1 = [
   {
     :name => "Costa Rica",
     :taxonomy => destinations,
@@ -57,7 +61,15 @@ taxons = [
 
 ]
 
-taxons_2 = [
+
+taxons.each do |taxon_attrs|
+  if taxon_attrs[:parent]
+    taxon_attrs[:parent] = Spree::Taxon.find_by_name!(taxon_attrs[:parent])
+    Spree::Taxon.create!(taxon_attrs)
+  end
+end
+
+taxons = [
   {
     :name => "Unied Stated",
     :taxonomy => destinations,
@@ -118,10 +130,10 @@ taxons_2 = [
   },
 ]  
 
+     
 taxons.each do |taxon_attrs|
   if taxon_attrs[:parent]
     taxon_attrs[:parent] = Spree::Taxon.find_by_name!(taxon_attrs[:parent])
     Spree::Taxon.create!(taxon_attrs)
   end
 end
-     
