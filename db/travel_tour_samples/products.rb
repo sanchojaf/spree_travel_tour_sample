7598f1e3cc6965f331913f3952f546a87506fffb
Miguel Sancho <sanchojaf@gmail.com>
2013-12-30 06:27:50 -0500

    add activity details

--- db/travel_tour_samples/products.rb
+++ db/travel_tour_samples/products.rb
@@ -1,10 +1,10 @@
 shipping_category = Spree::ShippingCategory.find_by_name!("Default")
 
-activity_detail = Spree::ActivityDetail.new({:duration => "#{3 + rand(5)} hours"
-                                                })
+activity_detail = Spree::ActivityDetail.new
+activity_detail."#{3 + rand(5)} hours"
 activity_detail.save
 
-puts "/////////////////////////////// activity_detail = {activity_detail.inspect} //////////////"
+puts "/////////////////////////////// activity_detail = #{activity_detail.inspect} //////////////"
 
 default_attrs = { 
   :activity_detail => activity_detail,
