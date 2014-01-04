acbb8ee79fb0168f0377b085a3f6632bd9465a5c
Miguel Sancho <sanchojaf@gmail.com>
2014-01-03 23:18:41 -0500

    change shipping assigment

--- db/travel_tour_samples/products.rb
+++ db/travel_tour_samples/products.rb
@@ -1,3 +1,4 @@
+default_shipping_category = Spree::ShippingCategory.find_by_name!("Default")
 
 user_admin = Spree::User.where(:email => "spree@example.com")
 user_jack = Spree::User.where(:email => "jack@g.com")
@@ -13,97 +14,113 @@ products = [
     :name => "Ruby on Rails Tote",
     :price => 15.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Ruby on Rails Bag",
     :price => 22.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.first.id
+    :user_id => user_jack.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Ruby on Rails Baseball Jersey",
     :price => 17.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Ruby on Rails Jr. Spaghetti",
     :price => 23.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.first.id
+    :user_id => user_jack.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Ruby on Rails Ringer T-Shirt",
     :price => 25.60,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Ruby Baseball Jersey",
     :price => 29.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.first.id
+    :user_id => user_jack.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Apache Baseball Jersey",
     :price => 9.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Spree Baseball Jersey",
     :price => 15.75,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.first.id
+    :user_id => user_jack.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Spree Jr. Spaghetti",
     :price => 18.90,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.first.id
+    :user_id => user_jack.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Spree Ringer T-Shirt",
     :price => 21.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Spree Tote",
     :price => 15.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Spree Bag",
     :price => 12.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Ruby on Rails Mug",
     :price => 13.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Ruby on Rails Stein",
     :price => 16.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Spree Stein",
     :price => 16.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_admin.first.id
+    :user_id => user_admin.first.id, 
+    :shipping_category = default_shipping_category
   },
   {
     :name => "Spree Mug",
     :price => 13.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.first.id
+    :user_id => user_sa.first.id, 
+    :shipping_category = default_shipping_category
   }
 ]
 
@@ -111,12 +128,11 @@ products.each do |product_attrs|
 #  eur_price = product_attrs.delete(:eur_price)
   Spree::Config[:currency] = "USD"
 
-  default_shipping_category = Spree::ShippingCategory.find_by_name!("Default")
+
   product = Spree::Product.create!(default_attrs.merge(product_attrs))
 #  Spree::Config[:currency] = "EUR"
 #  product.reload
 #  product.price = eur_price
-  product.shipping_category = default_shipping_category
                    
   activity_detail = Spree::ActivityDetail.new
   activity_detail.duration = "#{3 + rand(5)} hours"
