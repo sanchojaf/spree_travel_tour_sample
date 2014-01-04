0efdbb11d6434c8af37cfd2290c8bdf5142dfa53
Miguel Sancho <sanchojaf@gmail.com>
2014-01-03 23:08:50 -0500

    resolved issue with id assigment

--- db/travel_tour_samples/products.rb
+++ db/travel_tour_samples/products.rb
@@ -13,97 +13,97 @@ products = [
     :name => "Ruby on Rails Tote",
     :price => 15.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Ruby on Rails Bag",
     :price => 22.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.id
+    :user_id => user_jack.first.id
   },
   {
     :name => "Ruby on Rails Baseball Jersey",
     :price => 17.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Ruby on Rails Jr. Spaghetti",
     :price => 23.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.id
+    :user_id => user_jack.first.id
   },
   {
     :name => "Ruby on Rails Ringer T-Shirt",
     :price => 25.60,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Ruby Baseball Jersey",
     :price => 29.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.id
+    :user_id => user_jack.first.id
   },
   {
     :name => "Apache Baseball Jersey",
     :price => 9.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Spree Baseball Jersey",
     :price => 15.75,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.id
+    :user_id => user_jack.first.id
   },
   {
     :name => "Spree Jr. Spaghetti",
     :price => 18.90,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_jack.id
+    :user_id => user_jack.first.id
   },
   {
     :name => "Spree Ringer T-Shirt",
     :price => 21.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Spree Tote",
     :price => 15.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Spree Bag",
     :price => 12.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Ruby on Rails Mug",
     :price => 13.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Ruby on Rails Stein",
     :price => 16.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   },
   {
     :name => "Spree Stein",
     :price => 16.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_admin.id
+    :user_id => user_admin.first.id
   },
   {
     :name => "Spree Mug",
     :price => 13.99,
     :description => Faker::Lorem.paragraph,
-    :user_id => user_sa.id
+    :user_id => user_sa.first.id
   }
 ]
 
